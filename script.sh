ec4613d27883654954bc402f9533d144d6f5356a
Soberat <soberat1@gmail.com>
2021-05-08 20:49:25 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,12 +10,11 @@ fi
 echo -e "\033[1;38;5;118mBSK lab6 fix by Mirek Wiacek B)\033[0m"
 
 echo -e "\033[1;36m>>>>Installing dependencies...\033[0m"
-apt-get install pkg-config libnl-3-dev libnl-genl-3-dev asleap
+apt-get -y install pkg-config libnl-3-dev libnl-genl-3-dev asleap
 
 echo -e "\033[1;36m>>>>Installing OpenSSL...\033[0m"
-apt-get install libssl-dev
-apt-get install libssl1.0-dev
-
+apt-get -y install libssl-dev
+apt-get -y install libssl1.0-dev
 echo -e "\033[1;36m>>>>Patching OpenSSL...\033[0m"
 sed -i 's/MinProtocol = TLSv1.\b[0-9]\{1\}\b/MinProtocol = TLSv1.0/g'  '/etc/ssl/openssl.cnf'
 
