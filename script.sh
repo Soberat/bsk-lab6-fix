ef5cb2771bec0b6caee94fc51b4fcf425a7812c4
Soberat <soberat1@gmail.com>
2021-05-08 20:47:14 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # Author: Miroslaw Wiacek
 
+# ensure root
+if [ "$EUID" -ne 0 ]
+  then echo -e "\033[0;31mMialo byc \"sudo bash ./script.sh \" :|\033[0m"
+  exit
+fi
+
 echo -e "\033[1;38;5;118mBSK lab6 fix by Mirek Wiacek B)\033[0m"
 
 echo -e "\033[1;36m>>>>Installing dependencies...\033[0m"
@@ -9,12 +15,6 @@ apt-get install pkg-config libnl-3-dev libnl-genl-3-dev asleap
 echo -e "\033[1;36m>>>>Installing OpenSSL...\033[0m"
 apt-get install libssl-dev
 apt-get install libssl1.0-dev
-
-# ensure root
-if [ "$EUID" -ne 0 ]
-  then echo -e "\033[0;31mMialo byc \"sudo bash ./script.sh \" :|\033[0m"
-  exit
-fi
 
 echo -e "\033[1;36m>>>>Patching OpenSSL...\033[0m"
 sed -i 's/MinProtocol = TLSv1.\b[0-9]\{1\}\b/MinProtocol = TLSv1.0/g'  '/etc/ssl/openssl.cnf'
